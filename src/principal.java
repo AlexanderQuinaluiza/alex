814ab14df4a1f6ce9aa5842222da80435ef14c1b
PC2012 <PC2012@PC2012-PC>
2014-04-23 15:22:13 -0500

    nueva version borrando lineas nuevas

--- src/principal.java
+++ src/principal.java
@@ -1,14 +1,14 @@
 
 public class principal {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //hola amigos
 		
 		// somos uno solo en el mundo
-	}
+	
+	//nosotros somos nuevos en esto
+		}
 
 }
